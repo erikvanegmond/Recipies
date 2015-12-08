486828c56b0d08a777840502f512f13c5720e5ff
CKettler <celestekettler@gmail.com>
2015-12-08 14:02:27 +0100

    description of the probability model

--- recipe.py
+++ recipe.py
@@ -286,6 +286,13 @@ class Recipe(object):
         return decorate
 
 
+#P(C) =prod{i} P(gi,vi) prod{i} prod{cp in di} P(o(cp)|d1...di1,c1...cp1,gi)
+# verbsignature: DOBJ,PP,both and Origin = 0 or not
+#P(R|C) = prod{i} P(vi|gi) prod{skij in Sij} funcsem(o,(part)ij) P(skij|syn,sem,C,hi)
+#When the argument is a food, and o != 0 : prod{k} P(skij|food(skij,C)) P(implicit|d(i,o))
+#When the argument is a food, and o = 0 : prod{l} (wl|rsw)
+#When the argument is a location : explicit :(loc(i),loc(o))   implicit :P(loc(o)|vi)
+
 # amishMeatloaf = Recipe("..\\AllRecipesData\\chunked\\BeefMeatLoaf-chunked\\amish-meatloaf.txt")
 # pkl_file = open('globals.pkl', 'r')
 # global_verb_count = pickle.load(pkl_file)
